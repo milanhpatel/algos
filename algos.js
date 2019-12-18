369ca149e24af218f0605755f6d2c863db564d34
Milan Patel <milanpatel92@gmail.com>
2019-12-18 13:52:13 -0500

    first commit

---
+++ algos.js
@@ -0,0 +1,11 @@
+function twoNumberSum(array, targetSum) {
+  // Write your code here.
+	for(i=0; i < array.length, i++) {
+		if(targetSum-array[i] === )
+	}
+}
+	
+// Do not edit the line below.
+exports.twoNumberSum = twoNumberSum;
+
+// store each number in the array in a hash table
